d13643286797db433e7d6da4f74820a43ab31cb2
neohed <35173474+neohed@users.noreply.github.com>
2022-04-15 14:20:35 +0100

    Update new.tsx

--- blog-tutorial/app/routes/posts/admin/new.tsx
+++ blog-tutorial/app/routes/posts/admin/new.tsx
@@ -1,10 +1,11 @@
 import type { ActionFunction } from "@remix-run/node";
 import type {ActionData} from "./types";
+import type { Post } from "~/models/post.server";
 import { json, redirect } from "@remix-run/node";
 import { Form, useActionData, useTransition } from "@remix-run/react";
 import invariant from "tiny-invariant";
-
 import { createPost } from "~/models/post.server";
+import PostForm from "~/routes/posts/admin/PostForm";
 
 export const action: ActionFunction = async ({request}) => {
   //await new Promise((res) => setTimeout(res, 1000));
@@ -45,8 +46,6 @@ export const action: ActionFunction = async ({request}) => {
   return redirect("/posts/admin");
 };
 
-const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
-
 export default function NewPost() {
   const errors = useActionData();
 
@@ -55,50 +54,11 @@ export default function NewPost() {
 
   return (
     <Form method="post">
-      <p>
-        <label>
-          Post Title:{" "}
-          {errors?.title ? (
-            <em className="text-red-600">{errors.title}</em>
-          ) : null}
-          <input type="text" name="title" className={inputClassName} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Post Slug:{" "}
-          {errors?.slug ? (
-            <em className="text-red-600">{errors.slug}</em>
-          ) : null}
-          <input type="text" name="slug" className={inputClassName} />
-        </label>
-      </p>
-      <p>
-        <label htmlFor="markdown">
-          Markdown:{" "}
-          {errors?.markdown ? (
-            <em className="text-red-600">
-              {errors.markdown}
-            </em>
-          ) : null}
-        </label>
-        <br />
-        <textarea
-          id="markdown"
-          rows={20}
-          name="markdown"
-          className={`${inputClassName} font-mono`}
-        />
-      </p>
-      <p className="text-right">
-        <button
-          type="submit"
-          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
-          disabled={isCreating}
-        >
-          {isCreating ? "Creating..." : "Create Post"}
-        </button>
-      </p>
+      <PostForm
+        post={{} as Post}
+        errors={errors}
+        isInProgress={isCreating}
+      />
     </Form>
   );
 }

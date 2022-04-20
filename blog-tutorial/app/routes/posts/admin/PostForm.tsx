8b73c4ff80af7ba762f57d80029388ec4850567b
neohed <35173474+neohed@users.noreply.github.com>
2022-04-20 13:28:52 +0100

    Update PostForm.tsx

--- blog-tutorial/app/routes/posts/admin/PostForm.tsx
+++ blog-tutorial/app/routes/posts/admin/PostForm.tsx
@@ -4,7 +4,7 @@ import type { Post } from "~/models/post.server";
 import { FormTypes } from "~/lib/forms";
 import useShallowCopy from "~/lib/useShallowCopy";
 import TextInput from "~/components/TextInput";
-import { Form } from "@remix-run/react";
+import { Form, useTransition } from "@remix-run/react";
 
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
@@ -15,82 +15,88 @@ interface PostFormProps {
   formType: FormTypes,
 }
 
-const PostForm = ({post, errors, isInProgress = false, formType}: PostFormProps) => {
+const PostForm = ({ post, errors, isInProgress = false, formType }: PostFormProps) => {
   const [postEntity, isDirty, mutate] = useShallowCopy(post);
+  const transition = useTransition();
 
   return (
     <Form method="post">
-      <p className='text-red-700'>
-        {
-          isDirty
-            ? '* Unsaved'
-            : ' '
-        }
-      </p>
-      <input type="hidden" name='formType' value={formType} />
-      <p>
-        <label>
-          Post Title:{" "}
-          {errors?.title ? (
-            <em className="text-red-600">{errors.title}</em>
-          ) : null}
-          <TextInput
-            id='title'
-            className={inputClassName}
-            value={postEntity.title}
-            changeHandler={title => mutate({title})}
+      <fieldset
+        disabled={transition.state === "submitting"}
+      >
+        <p className="text-red-700">
+          {
+            isDirty
+              ? "* Unsaved"
+              : " "
+          }
+        </p>
+        <input type="hidden" name="formType" value={formType} />
+        <p>
+          <label>
+            Post Title:{" "}
+            {errors?.title ? (
+              <em className="text-red-600">{errors.title}</em>
+            ) : null}
+            <TextInput
+              id="title"
+              className={inputClassName}
+              value={postEntity.title}
+              changeHandler={title => mutate({ title })}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Post Slug:{" "}
+            {errors?.slug ? (
+              <em className="text-red-600">{errors.slug}</em>
+            ) : null}
+            <TextInput
+              id="slug"
+              className={inputClassName}
+              value={postEntity.slug}
+              changeHandler={slug => mutate({ slug })}
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="markdown">
+            Markdown:{" "}
+            {errors?.markdown ? (
+              <em className="text-red-600">
+                {errors.markdown}
+              </em>
+            ) : null}
+          </label>
+          <br />
+          <textarea
+            id="markdown"
+            rows={20}
+            name="markdown"
+            className={`${inputClassName} font-mono`}
+            value={postEntity.markdown || ""}
+            onChange={({ target }) => mutate({ markdown: target.value })}
           />
-        </label>
-      </p>
-      <p>
-        <label>
-          Post Slug:{" "}
-          {errors?.slug ? (
-            <em className="text-red-600">{errors.slug}</em>
-          ) : null}
-          <TextInput
-            id='slug'
-            className={inputClassName}
-            value={postEntity.slug}
-            changeHandler={slug => mutate({slug})}
-          />
-        </label>
-      </p>
-      <p>
-        <label htmlFor="markdown">
-          Markdown:{" "}
-          {errors?.markdown ? (
-            <em className="text-red-600">
-              {errors.markdown}
-            </em>
-          ) : null}
-        </label>
-        <br />
-        <textarea
-          id="markdown"
-          rows={20}
-          name="markdown"
-          className={`${inputClassName} font-mono`}
-          value={postEntity.markdown || ''}
-          onChange={({target}) => mutate({markdown: target.value})}
-        />
-      </p>
-      <div className="flex justify-between">
-        {
-          formType === FormTypes.edit && <button
-            className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300 right-auto"
+        </p>
+        <div className="flex justify-between">
+          {
+            formType === FormTypes.edit && <button
+              value={postEntity.slug}
+              className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300 right-auto"
+            >
+              Delete
+            </button>
+          }
+          <button
+            type="submit"
+            className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+            disabled={isInProgress}
           >
-            Delete
+            {isInProgress ? "Saving..." : "Save"}
           </button>
-        }
-        <button
-          type="submit"
-          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
-          disabled={isInProgress}
-        >
-          {isInProgress ? "Saving..." : "Save"}
-        </button>
-      </div>
+        </div>
+      </fieldset>
     </Form>
   );
 };
